50b7995078e0f28f9fdb4980a9896513209d7bd0
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-05-18 20:38:02 -0500

    Add files via upload

---
+++ May18.js
@@ -0,0 +1,41 @@
+/* viudeo 22 */
+
+const airline = 'TAP Air Jazmine'
+const plane = 'A320'
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lasti]IndexOf('r'));
+console.log(airline.indexOf('Jazmine'));
+
+console.log(airline.slice(4));
+console.log(slice(4,7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.lastIndexOf(' ') + 1)\);
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+const checkMiddleSeat = function(seat) {
+const s = seat.slice(-1)
+};
+ 
+const s = seat.slice(-1);
+if (s === 'B' || s === 'E') console.log('You got the middle seat')
+
+
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+/* Video 123 */
+
+
